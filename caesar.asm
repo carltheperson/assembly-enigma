88560d9f57f82d0a2f12ca451d0a11e2a277b13e
Carl Riis <carl.aagot.riis@gmail.com>
2022-07-14 08:28:49 +0200

    Cleaned up caesar

--- caesar.asm
+++ caesar.asm
@@ -2,6 +2,8 @@
 
 #include <stdio.s>
 
+offset = 5
+
 LDA #<ISR
 STA $FFFE
 LDA #>ISR
@@ -12,54 +14,55 @@ loop:
 	wai
   JMP loop
 
-LDX #$0 ; We use X to keep state; 0=null, 1=encrypt, 2=decrypt
+; We use X to keep state; 0=null, 1=encrypt, 2=decrypt
+LDX #$0 ; We initialize it to 0
 
-ISR pha ; Start ISR
+ISR pha
 
 ; --------- Setup end --------- 
 
 start:
-	CPX #$0 ; If we have not decided if encrypt or decrypt
+	CPX #$0 									; If user has not yet decided if encrypt or decrypt; X = 0
 	BEQ compare_and_set_flag
-	CPX #$1 ; If we have decided encrypt
+	CPX #$1									  ; If user has decided encrypt; X = 1
 	BEQ encrypt
-	CPX #$2 ; If we have decided decrypt
+	CPX #$2 									; If user has decided decrypt; X = 2
 	BEQ decrypt
 
 compare_and_set_flag:
 	LDX getc
-	CPX #"e"
+	CPX #"e"							; User has decided encrypt
 	BEQ set_encrypt_flag
-	CPX #"d"
+	CPX #"d"							; User has decided decrypt
 	BEQ set_decrypt_flag
-	JMP error
+	JMP error							; User did not pick "e" or "d". Displaying "ERROR"
 
 set_encrypt_flag:
-	LDX #$1
+	LDX #$1						; Setting X to 1
 	JMP done
 
 set_decrypt_flag:
-	LDX #$2
+	LDX #$2						; Setting X to 2
 	JMP done
 
 encrypt:
-	LDA getc
-	CLC
-	ADC #4
-	STA putc
+	LDA getc					; Get typed char into A
+	CLC								; Clear carry flag
+	ADC #offset				; Add our offset to A
+	STA putc					; Print char
 	JMP done
 
 decrypt:
-	LDA getc
-	SBC #4
-	STA putc
+	LDA getc					; Get typed char into A
+	SBC #offset				; Subtract our offset from A
+	STA putc					; Print char
 
-done:
+done:								; This will loop back to start
 	LDA getc
 	pla
 	rti
 
-error:
+error:							; Print "ERROR"
 		LDA #"E"
 		STA putc
 		LDA #"R"
