cc08dabe40553cbf6db79f743ed43a447850c708
Carl Riis <carl.aagot.riis@gmail.com>
2022-07-23 19:49:44 +0200

    Delete enigma.ts

--- enigma.ts
+++
@@ -1,5 +0,0 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-const r1Letters = "BDFHJLCPRTXVZNYEIHGAKMUSQO".split("");
-const r1 = r1Letters.map((l) => letters.indexOf(l));
-
-console.log(r1);
